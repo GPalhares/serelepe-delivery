fc804163966f481c468793c0de695db371c6a164
Gabriel <devpalhares@gmail.com>
2023-05-17 18:24:39 -0300

    feat: Admin page

--- back-end/src/routes/adminRoutes.js
+++ back-end/src/routes/adminRoutes.js
@@ -2,13 +2,13 @@ const { Router } = require('express');
 const userController = require('../controllers/userController');
 const registerController = require('../controllers/registerController');
 const { validateRegister } = require('../middlewares/validateInformation');
-// const { validateAdmin } = require('../middlewares/validateAdminRole');
-// const { validateToken } = require('../middlewares/validateToken');
+const { validateAdmin } = require('../middlewares/validateAdminRole');
+const { validateToken } = require('../middlewares/validateToken');
 
 const routes = Router();
 
-// routes.use(validateToken);
-// routes.use(validateAdmin);
+routes.use(validateToken);
+routes.use(validateAdmin);
 
 routes.post('/', validateRegister, registerController.userRegister);
 routes.get('/', userController.getAllUsers);
